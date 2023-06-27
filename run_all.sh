5a0116f6f8f741c8e5b7650fd5bbf8c546e32062
Jisu Yu <det3652000@naver.com>
2023-06-27 18:11:43 +0900

    Chore: Improve `run_all.sh` would check `run.sh` exists

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+RUN_SCRIPT="run.sh"
+
 for dir in */; do
     pushd $dir
-    ./run.sh
-    printf "\n\n"
+    if [ -x "$RUN_SCRIPT" ]; then
+        ./$RUN_SCRIPT
+    else
+        echo "ERROR: Cannot find $RUN_SCRIPT in $dir"
+    fi
     popd
 done
