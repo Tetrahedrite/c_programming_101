22d4b9a11f1386094e42945a34f0e1c21a3363fc
Jisu Yu <det3652000@naver.com>
2023-07-20 00:15:55 +0900

    Fix: Wrong algorithm for test leap year

--- ch5/5_p04.c
+++ ch5/5_p04.c
@@ -8,11 +8,12 @@ int main() {
 
     if (year % 4 == 0)
         if (year % 100 == 0)
-            printf("%d년은 윤년이 아닙니다\n", year);
+            if (year % 400 == 0)
+                printf("%d년은 윤년입니다\n", year);
+            else
+                printf("%d년은 윤년이 아닙니다\n", year);
         else
             printf("%d년은 윤년입니다\n", year);
-    else if (year % 100 == 0 && year % 400 == 0)
-        printf("%d년은 윤년입니다\n", year);
     else
         printf("%d년은 윤년이 아닙니다\n", year);
 
