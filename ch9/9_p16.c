25c8633a891820a9ea510ed5183e78b789ed833e
Jisu Yu <det3652000@naver.com>
2023-10-23 23:00:29 +0900

    Refactor: Chapter 9 - Programming Assignment - Problem 16 ~ 17 - Split main to multiple function

--- ch9/9_p16.c
+++ ch9/9_p16.c
@@ -3,6 +3,23 @@
 
 #define STR_SIZE 1024
 
+#define ACCOUNT_COMPARE_SUCCESS 0
+#define ACCOUNT_NOT_EXIST_ID 1
+#define ACCOUNT_WRONG_PASSWORD 2
+
+int login(const char *(*account_db)[2], int db_record_size, const char *username, const char *password) {
+    int bad_password = 0;
+    for (int i = 0; i < db_record_size; i++) {
+        int match_id = (strcmp(account_db[i][0], username) == 0);
+        int match_pw = (strcmp(account_db[i][1], password) == 0);
+        if (match_id && match_pw)
+            return ACCOUNT_COMPARE_SUCCESS;
+        else if (match_id)
+            return ACCOUNT_WRONG_PASSWORD;
+    }
+    return ACCOUNT_NOT_EXIST_ID;
+}
+
 int main() {
     const char *account_db[][2] = {
         { "parkjimin", "@BestOfMe@" },
@@ -23,25 +40,18 @@ int main() {
         fgets(pw, STR_SIZE, stdin);
         pw[strlen(pw) - 1] = '\0';
 
-        int bad_password = 0;
-        for (int i = 0; i < sizeof(account_db) / sizeof(account_db[0]); i++) {
-            int match_id = (strcmp(account_db[i][0], id) == 0);
-            int match_pw = (strcmp(account_db[i][1], pw) == 0);
-            if (match_id && match_pw) {
+        switch (login(account_db, sizeof(account_db) / sizeof(account_db[0]), id, pw)) {
+            case ACCOUNT_COMPARE_SUCCESS:
                 puts("로그인 성공!");
                 return 0;
-            } else if (match_id && !match_pw) {
-                bad_password = 1;
+            case ACCOUNT_NOT_EXIST_ID:
+                puts("해당 id가 없습니다!");
+                fail_count++;
                 break;
-            }
-        }
-        
-        if (bad_password) {
-            puts("패스워드가 틀렸습니다.");
-            fail_count++;
-        } else {
-            puts("해당 id가 없습니다!");
-            fail_count++;
+            case ACCOUNT_WRONG_PASSWORD:
+                puts("패스워드가 틀렸습니다.");
+                fail_count++;
+                break;
         }
 
         if (fail_count >= 3) {
--- ch9/9_p17.c
+++ ch9/9_p17.c
@@ -6,9 +6,48 @@
 #define SONG_COUNT 20
 #define SONG_NAME_SIZE (40 + 1)
 
+static void add_song(char (*songs)[SONG_NAME_SIZE], int *phead) {
+    char str[STR_SIZE];
+    if (*phead < SONG_COUNT) {
+        printf("노래 제목? ");
+        fgets(str, STR_SIZE, stdin);
+        str[strlen(str) - 1] = '\0';
+        strcpy(songs[(*phead)++], str);
+    } else {
+        puts("최대 20개까지만 추가할 수 있습니다.");
+    }
+}
+
+static void modify_song_name(char (*songs)[SONG_NAME_SIZE], int head) {
+    char str[STR_SIZE];
+    printf("찾을 노래 제목? ");
+    fgets(str, STR_SIZE, stdin);
+    str[strlen(str) - 1] = '\0';
+
+    int found = 0;
+    for (int i = 0; i < head; i++) {
+        if (strcmp(songs[i], str) == 0) {
+            printf("수정할 제목? ");
+            fgets(str, STR_SIZE, stdin);
+            str[strlen(str) - 1] = '\0';
+            strcpy(songs[i], str);
+            found = 1;
+            break;
+        }
+    }
+    if (!found)
+        puts("노래 제목을 찾을 수 없습니다.");
+}
+
+static void list_songs(char (*songs)[SONG_NAME_SIZE], int head) {
+    puts("<< 노래 제목 >>");
+    for (int i = 0; i < head; i++) {
+        puts(songs[i]);
+    }
+}
+
 int main() {
     char songs[SONG_COUNT][SONG_NAME_SIZE];
-    char str[STR_SIZE];
     unsigned int menu, song_head = 0;
 
     while (1) {
@@ -20,39 +59,13 @@ int main() {
             case 0: // 종료
                 return 0;
             case 1: // 추가
-                if (song_head < SONG_COUNT) {
-                    printf("노래 제목? ");
-                    fgets(str, STR_SIZE, stdin);
-                    str[strlen(str) - 1] = '\0';
-                    strcpy(songs[song_head++], str);
-                } else {
-                    puts("최대 20개까지만 추가할 수 있습니다.");
-                }
+                add_song(songs, &song_head);
                 break;
             case 2: // 수정
-                printf("찾을 노래 제목? ");
-                fgets(str, STR_SIZE, stdin);
-                str[strlen(str) - 1] = '\0';
-
-                int found = 0;
-                for (int i = 0; i < song_head; i++) {
-                    if (strcmp(songs[i], str) == 0) {
-                        printf("수정할 제목? ");
-                        fgets(str, STR_SIZE, stdin);
-                        str[strlen(str) - 1] = '\0';
-                        strcpy(songs[i], str);
-                        found = 1;
-                        break;
-                    }
-                }
-                if (!found)
-                    puts("노래 제목을 찾을 수 없습니다.");
+                modify_song_name(songs, song_head);
                 break;
             case 3: // 목록
-                puts("<< 노래 제목 >>");
-                for (int i = 0; i < song_head; i++) {
-                    puts(songs[i]);
-                }
+                list_songs(songs, song_head);
                 break;
             default:
                 puts("잘못된 입력입니다.");
