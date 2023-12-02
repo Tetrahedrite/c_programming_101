337d7c4d11abad933a4a6f07b28c4f466fbf38e3
Jisu Yu <det3652000@naver.com>
2023-12-03 02:35:32 +0900

    Feat: Improve time comparison shell script to check and make executable

--- ch11/compare_p03_p04.sh
+++ ch11/compare_p03_p04.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
+X_BEFORE_SOURCE="11_p03"
+X_AFTER_SOURCE="11_p04"
+
 X_BEFORE="11_p03.out"
 X_AFTER="11_p04.out"
+
+if ! [ -x "$X_BEFORE" ] || ! [ -x "$X_AFTER" ]; then
+    echo "There is no $X_BEFORE and $X_AFTER... Try to make..."
+    make "$X_BEFORE_SOURCE" "$X_AFTER_SOURCE"
+fi
 
 if [ -x "$X_BEFORE" ] && [ -x "$X_AFTER" ]; then
     echo "Executing $X_BEFORE..."
     time ./$X_BEFORE
     echo "Executing $X_AFTER..."
     time ./$X_AFTER
+else
+    echo "Fatal error: failed to find $X_BEFORE and $X_AFTER"
 fi
